be4d5ee153958bb298825069c8629247e85d85e7
Martijn Faassen <faassen@startifact.com>
2025-05-29 21:24:11 +0200

    Make decompression work.

--- src/usage/bitpacking_builder.rs
+++ src/usage/bitpacking_builder.rs
@@ -5,11 +5,22 @@ use crate::{info::NodeInfoId, lookup::NodeLookup};
 use super::{EliasFanoUsageIndex, UsageBuilder};
 
 #[derive(Clone)]
+struct BlockInfo {
+    // the initial value for this block
+    initial_value: Option<u32>,
+    num_bits: u8,
+    compressed_start: usize,
+    compressed_len: usize,
+}
+
+#[derive(Clone)]
 struct Packed {
     compressed: Vec<u8>,
     remainder: Vec<u32>,
+    block_infos: Vec<BlockInfo>,
     used: usize,
-    last_initial_value: Option<u32>,
+    // the initial value we are to use for the next block
+    initial_value: Option<u32>,
 }
 
 impl Packed {
@@ -17,8 +28,9 @@ impl Packed {
         Self {
             compressed: Vec::new(),
             remainder: Vec::new(),
+            block_infos: Vec::new(),
             used: 0,
-            last_initial_value: Some(0),
+            initial_value: None,
         }
     }
 
@@ -35,8 +47,8 @@ impl Packed {
         }
         // now pack the remainder into a single block
         let bitpacker = BitPacker4x::new();
-        let num_bits: u8 =
-            bitpacker.num_bits_strictly_sorted(self.last_initial_value, &self.remainder);
+
+        let num_bits: u8 = bitpacker.num_bits_strictly_sorted(self.initial_value, &self.remainder);
 
         // we have reached the block size, so we can compress the
         // next block. first reserve enough space in compressed
@@ -44,17 +56,43 @@ impl Packed {
         self.compressed
             .resize(compressed_start + 4 * BitPacker4x::BLOCK_LEN, 0);
         let compressed_len = bitpacker.compress_strictly_sorted(
-            self.last_initial_value,
+            self.initial_value,
             &self.remainder,
             &mut self.compressed[compressed_start..],
             num_bits,
         );
+        self.block_infos.push(BlockInfo {
+            initial_value: self.initial_value,
+            compressed_start,
+            compressed_len,
+            num_bits,
+        });
         // we now determine how much packed space we actually used
         self.used += compressed_len;
-        // new initial value is the last value in the remainder
-        self.last_initial_value = self.remainder.last().cloned();
+        // update the initial value to the last bit of the remainer
+        self.initial_value = self.remainder.last().cloned();
         // now we can clear the remainder
         self.remainder.clear();
+    }
+
+    fn decompressed(&self) -> Vec<u32> {
+        let mut decompressed_data = Vec::new();
+        for block_info in &self.block_infos {
+            // add enough space for another block
+            let decompressed_start = decompressed_data.len();
+            decompressed_data.resize(decompressed_start + BitPacker4x::BLOCK_LEN, 0);
+            let bitpacker = BitPacker4x::new();
+            bitpacker.decompress_strictly_sorted(
+                block_info.initial_value,
+                &self.compressed[block_info.compressed_start
+                    ..block_info.compressed_start + block_info.compressed_len],
+                &mut decompressed_data[decompressed_start..],
+                block_info.num_bits,
+            );
+        }
+        // now we add the remainder
+        decompressed_data.extend(&self.remainder);
+        decompressed_data
     }
 }
 
@@ -131,10 +169,14 @@ mod tests {
 
     #[test]
     fn test_packed() {
-        let mut packed = Packed::new();
         let size = 10000usize;
+        let mut initial_data = Vec::new();
         for i in 0..size {
-            packed.append((i * 2) as u32);
+            initial_data.push((i * 2) as u32);
+        }
+        let mut packed = Packed::new();
+        for value in &initial_data {
+            packed.append(*value);
         }
         let original_size = size * 4;
         let compressed_size = packed.compressed.len();
@@ -143,5 +185,7 @@ mod tests {
             "Compressed {} values, original size: {}, compressed size: {}, difference: {}",
             size, original_size, compressed_size, difference
         );
+        let decompressed_data = packed.decompressed();
+        assert_eq!(initial_data, decompressed_data)
     }
 }
